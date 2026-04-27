7c8b40cd7149a894553c299a2048e09ae1d2880f
Mattyudha <dewarahmat12334@gmail.com>
2026-04-28 01:43:15 +0700

    fix: db host in seeder

--- run_seed.sh
+++ run_seed.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Menjalankan seeder admin..."
-sudo docker run --rm --network host -v "$PWD:/app" -w /app golang:alpine sh -c "go mod download && go run cmd/seed_admin/main.go"
+sudo docker run --rm --network host -e DB_HOST=127.0.0.1 -v "$PWD:/app" -w /app golang:alpine sh -c "go mod download && go run cmd/seed_admin/main.go"
 echo "Selesai!"
